832d325360c55cc7fc28ca8df44774440625eba1
DoganK01 <98788987+DoganK01@users.noreply.github.com>
2022-03-08 13:27:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -84,6 +84,7 @@ n_classes = 9
 
 model = models.Sequential([
     resize_and_rescale,
+    data_augmentation,
     layers.Conv2D(32, kernel_size = (3,3), activation='relu', input_shape=input_shape),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64,  kernel_size = (3,3), activation='relu'),
